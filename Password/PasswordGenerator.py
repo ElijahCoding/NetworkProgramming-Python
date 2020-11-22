f25c0f0ae394d5ec3a63a6959633dc6a75224e7a
ElijahCoding <cuiyeqing960904@gmail.com>
2020-11-22 21:41:11 +0300

    password generator

--- Password/PasswordGenerator.py
+++ Password/PasswordGenerator.py
@@ -1,7 +1,30 @@
 import random
 
 uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-lowercase = uppercase_letters.lower()
+lowercase_letters = uppercase_letters.lower()
 digits = '0123456789'
 symbols = "()[]{},:;.-_/\\?+*# "
 
+upper, lower, nums, syms = True, True, True, True
+
+all = ""
+
+if upper:
+    all += uppercase_letters
+if lower:
+    all += lowercase_letters
+if nums:
+    all += digits
+if syms:
+    all += symbols
+
+length = 40
+amount = 20
+
+seed = 'word'
+
+random.seed(seed)
+
+for x in range(amount):
+    password = "".join(random.sample(all, length))
+    print(password)
